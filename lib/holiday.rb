eb2db3681a7a99b8f30bb3f55b1a82ba347a5a39
Zachary <zbberman@yahoo.com>
2019-02-06 18:09:11 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -79,7 +79,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do | key, data |
-    puts key.to_s.capitalize
+    puts key.to_s.capitalize + ":"
     data.each do | attribute, value |
       puts attribute.to_s.capitalize + value 
     end 
