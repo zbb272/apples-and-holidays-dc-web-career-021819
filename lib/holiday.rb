09b6a582dbd8effaafcd2a62c5960b224f3ef47f
Zachary <zbberman@yahoo.com>
2019-02-06 18:04:38 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -81,7 +81,8 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do | key, data |
     puts key.to_s.capitalize
     data.each do | attribute, value |
-      put attribute.to_s.capitalize
+      print attribute.to_s.capitalize
+      value
     end 
   end 
 
