1ab729a89f2a2ec4b49edb8738f808dd206c24c7
Zachary <zbberman@yahoo.com>
2019-02-06 18:33:45 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,12 +18,18 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
+  
+  holiday_hash[:summer][:fourth_of_july][1]
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
+  
+  holiday_hash[:winter].each do | key, value |
+    value << supply
+  end 
 
 end
 
@@ -31,18 +37,20 @@ end
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
+  
+  holiday_hash[:spring][:memorial_day] << supply
 
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
-  # code here
+  holiday_hash[season] = {holiday_name => supply_array}
+  holiday_hash
   # remember to return the updated hash
 
-end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-
+  holiday_hash[:winter].values.flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
@@ -53,13 +61,33 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+  holiday_hash.each do | key, data |
+    puts key.to_s.capitalize + ":"
+    data.each do | attribute, value |
+      holiday_names = attribute.to_s.split("_")
+      holiday_names.each do | word |
+        word.capitalize!
+      end 
+      holiday_names = holiday_names.join(" ")
+      puts "  " + holiday_names + ": " + value.join(", ")
+    end 
+  end 
+end
 end
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+  holiday_names_array = []
+  
+  holiday_hash.each do | key, data |
+    data.each do | attribute, value |
+      if value.include?("BBQ")
+        holiday_names_array << attribute
+      end 
+    end 
+  end 
+  holiday_names_array
 end
 
 
